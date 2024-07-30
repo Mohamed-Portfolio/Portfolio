bb6d158561bc0a7d000efbfb7051e7ebd9964e69
mohamedmrabet <mohamed10021995@gmail.com>
2024-07-30 17:55:54 +0100

    final

--- src/app/portfolio-website/about/about.component.ts
+++ src/app/portfolio-website/about/about.component.ts
@@ -6,20 +6,25 @@ import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent,CommonModule],
+  imports: [NavbarComponent, FooterComponent, CommonModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
-  showEducation : boolean =true
-  showProjects :boolean=true
-
-projects = [
-  {name:"project 1" , description : "hellooo"},
-  {name:"project 2" , description : "hellooo World"},
-  {name:"project 3" , description : "hellooo World benjamin"},
-]
+  showEducation: boolean = true;
+  showProjects: boolean = true;
+  showAbout_Me: boolean = true;
+  showSkills:boolean =true
 
 
+  projects = [
+    { name: "Ninja Hotel Reservation", description: "Website Application with React.js, Jwt, Cloudinary, Sequelize",Site: "https://github.com/mohamedmrabet/Hotel-Reservation"},
+    { name: "The Museum", description: "Website Application created with React.js, Jwt, Cloudinary", Site: "https://github.com/mohamedmrabet/RBKTN-05-23-Mvp-Starter" },
+    { name: "Hannibal-Market", description: "Website Application created with Next.js, TypeScript, and Tailwind",Site: "https://github.com/E-commerce-app-NextJS/firstRepo" },
+    { name: "Codini", description: "Website Application created with Next.js, TypeScript,Cloudinary,Strapi and Tailwind",Site: "https://github.com/Codini-Porject/Codini" }
+  ];
+
+  openSite(url: string) {
+    window.open(url, "_blank");
+  }
 }
-
